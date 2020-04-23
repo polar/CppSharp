20e845660871f1545b1c5e3564500d7d186e3451
Polar_Humenn <Polar_Humenn@trimble.com>
2020-04-23 17:23:43 -0400

    Fixing Std-symbols so that it includes the main headers as windows will generate template instantiations for std::allocator<xx>

--- src/Generator/Passes/PolarSymbolsStdCodeGenerator.cs
+++ src/Generator/Passes/PolarSymbolsStdCodeGenerator.cs
@@ -23,11 +23,34 @@ namespace CppSharp.Passes
             WriteLine("#define _LIBCPP_HIDE_FROM_ABI");
             NewLine();
 
+            System.Console.WriteLine($"Creating Std-symbols.cpp");
             if (TranslationUnit.Module == Options.SystemModule)
             {
+                System.Console.WriteLine("#include <string>");
+                System.Console.WriteLine("#include <vector>");
                 WriteLine("#include <string>");
                 WriteLine("#include <vector>");
             }
+            WriteLine("#include \"{0}\"", ((PolarDriverOptions)Context.Options).VectorHolderPath);
+            WriteLine("#include \"{0}\"", ((PolarDriverOptions)Context.Options).OptionalPath);
+            // Windows will generate template specializations for the std::allocator. We need these. 
+            foreach (var module in Options.Modules)
+            {
+                if (module != Options.SystemModule && module.LibraryName != "Ext")
+                {
+                    System.Console.WriteLine($"From Module {module.LibraryName}");
+                    foreach (var header in module.Headers)
+                    {
+                        // The Ext library .cpp files got added to the main module, so we do not want those.
+                        if (!header.EndsWith(".cpp"))
+                        {
+                            System.Console.WriteLine($"#include \"{header}\"");
+                            WriteLine($"#include \"{header}\"");
+                        }
+                    }
+                }
+            }
+            System.Console.WriteLine($"Created Std-symbols.cpp");
             NewLine();
         }
     }
